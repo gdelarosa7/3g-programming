42738b7023ce6c45720b5ad64d0d7ab4338638b2
Guillermo De La Rosa Orea <87319828+gdelarosa7@users.noreply.github.com>
2021-08-04 15:04:57 -0500

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+// factorial con recurson
+int factorial(int numero){
+	printf("calculando factorial de %d\n", numero);
+	if(numero < 0){
+	  return -1;  // si es numero negativo 
+	 }else if(numero == 1 || numero == 0 ) // casos base  
+	   return 1;                           // para detener la recursion 
+	printf("res parcial: %d\n ", numero*factorial(numero-1));
+return numero*factorial(numero-1); 
+}
+
+
+int main(int argc, char *argv[]) {
+	
+	int n, res;
+	 n = 3;
+	 res = factorial(n);
+	 printf(" resultado final %d!= %d\n", n, res);
+	 
+	 // res = factorial(n);
+	// printf(" %d!= %d\n", 6, factorial(6));
+	
+	return 0;
+}
+
