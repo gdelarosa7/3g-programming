6135c14adafdccab5f4d806b26d7737a3553b8b9
Guillermo De La Rosa Orea <87319828+gdelarosa7@users.noreply.github.com>
2021-08-10 14:14:14 -0500

    Add files via upload

---
+++ arch1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+char caracter;
+
+int main(int argc, char *argv[]) {
+	
+	FILE *file_pointer = NULL;
+	
+	file_pointer = fopen("/Users/Dr.GdelaRosa/Desktop/PROGRAMACION/cuento.rtf","r");
+	if(file_pointer == NULL){
+		printf("Error: No se encuentra el archivo");
+		return -1;
+	}
+	printf("\nEl contenido del archivo de prueba es \n\n");
+	while((caracter = fgetc(file_pointer)) != EOF){
+		printf("%c",caracter);
+	}
+	fclose(file_pointer);
+
+		return 0;
+}
+
