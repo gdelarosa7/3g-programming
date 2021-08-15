cd21979a1bd3de043ba80531126f988f4cfb1c47
Guillermo De La Rosa Orea <87319828+gdelarosa7@users.noreply.github.com>
2021-08-15 11:50:13 -0500

    Add files via upload

---
+++ empleados2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	int n=1; /*Número del empleado*/
+	char nombre[80];
+	double salario;
+	FILE *pArchivo;
+	
+	pArchivo=fopen("/Users/Dr.GdelaRosa/Desktop/PROGRAMACION/empleados.txt","w");
+	
+	if(pArchivo!=NULL){
+		do{
+			printf("Introduzca el número de empleado: ");
+			scanf("%d",&n);
+			/*Solo seguimos si n es positivo, en otro caso
+			entenderemos que la lista ha terminado */
+			if(n>0){
+				printf("Introduzca el nombre del empleado: ");
+				scanf("%s",nombre);
+				printf("Introduzca el salario del empleado: ");
+				scanf("%lf",&salario);
+				fprintf(pArchivo,"%d\t%s\t%lf\n",n,nombre,salario);
+			}
+		}while(n>0);
+		fclose(pArchivo);
+	}
+	
+	
+	return 0;
+}
+
---
+++ empleados3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+/*
+Ejemplo (lectura de los datos almacenados con fprintf, los datos están 
+separados por un tabulador).
+*/
+
+int main(int argc, char *argv[]) {
+	
+	int n=1;
+	char nombre[80];
+	double salario;
+	FILE *pArchivo;
+	pArchivo=fopen("/Users/Dr.GdelaRosa/Desktop/PROGRAMACION/empleados.txt","r");
+	if(pArchivo!=NULL){
+		while(!feof(pArchivo)){
+			fscanf(pArchivo,"%d\t%s\t%lf\n",&n,nombre,&salario);
+			printf("%d\t%s\t%lf\n",n,nombre,salario);
+		}
+		fclose(pArchivo);
+		getch();
+	}
+	return 0;
+}
+
