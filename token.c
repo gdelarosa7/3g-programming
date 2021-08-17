d6f36067b1f2f2d29e85873b2ba8115836a370cb
Guillermo De La Rosa Orea <87319828+gdelarosa7@users.noreply.github.com>
2021-08-17 15:04:11 -0500

    Add files via upload

---
+++ token.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+char texto[]= "Este es un, ejemplo. de varios delimitadores";
+char delim[]=" ,.";
+char *token;
+	
+int main(int argc, char *argv[]) {
+	printf("Texto: %s\n", texto);
+	token = strtok(texto,delim);
+	do{
+		printf("%s\n", token);
+		token = strtok(NULL,delim);
+	} while(token != NULL);
+	
+	return 0;
+}
+
